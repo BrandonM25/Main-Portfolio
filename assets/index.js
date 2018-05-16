92f1897495a9d78aa15fd5c1f1e7c53a0e3dd0f2
Brandon Matthews <brandonmatthews1982@gmail.com>
2018-05-16 09:43:04 -0400

    built static site

---
+++ assets/index.js
@@ -0,0 +1,43 @@
+new WOW().init();
+
+
+//Chart
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["HTML5", "CSS3", "JavaScript", "ReactJS", "Node.js", "MongoDB"],
+        datasets: [{
+            label: 'Scale of 0-20',
+            data: [19, 19, 15, 10, 13, 10],
+            backgroundColor: [
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(255, 206, 86, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 206, 86, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+
+    
